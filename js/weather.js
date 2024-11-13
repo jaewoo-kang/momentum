71201ebd17f38e26aedd0ae51df3ee18ce7f3da2
jaewoo-kang <jw.kang@cmckorea.org>
2024-11-13 11:26:47 -0600

    weather.js revised

--- js/weather.js
+++ js/weather.js
@@ -1,14 +1,26 @@
 const API_KEY = "6fa1d8edd5cbe26fac93c8a8244aff3d";
 
-function onGeoOk(position){
+function onGeoOK(position){
     const lat = position.coords.latitude;
     const lon = position.coords.longitude;
-    console.log("You live in", lat,",",lon);
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
-    console.log(url);
-}
-function onGeoError() {
-    alert("Can't find you. No weather for you.");
+    const url =
+     `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`; // call API 
+
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        const weather = document.querySelector("#weather span:first-child");
+        const city = document.querySelector("#weather span:nth-child(2)");
+        const temp = document.querySelector("#weather span:last-child");
+        const name = data.name;
+        weather.innerText = data.weather[0].main;
+        city.innerText = name;
+        temp.innerText = `${Math.floor(data.main.temp)}˚C`;})
+
+    }
+
+function onGeoError(){
+    alert("Can't find you!");
 }
 
-navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
+navigator.geolocation.getCurrentPosition(onGeoOK,onGeoError);
